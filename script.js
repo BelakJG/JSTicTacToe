b8357f00b248da203832f36fa0be0233e79bbab0
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-04-01 16:54:05 -0400

    Added functions to test if game is over

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const gameBoard = (() => {
-    let board = new Array(9).fill(" ");
-    const pageBoard = document.querySelector("#ttt-board")
+    let board = new Array(9);
+    for (i = 0; i <= 8; i++) {
+        board[i] = i;
+    };
+
+    const pageBoard = document.querySelector("#ttt-board");
 
     const populateBoard = () => {
         for (let i = 0; i <= 8; i++) {
@@ -16,9 +20,29 @@ const gameBoard = (() => {
         board[index] = symbol
     };
 
-    return { populateBoard, makeMove }
+    const checkRows = () => {
+        return ((board[0] == board[1] && board[1] == board[2])
+                || (board[3] == board[4] && board[4] == board[5])
+                || (board[6] == board[7] && board[7] == board[8]));
+    };
+
+    const checkcols = () => {
+        return ((board[0] == board[3] && board[3] == board[6])
+                || (board[1] == board[4] && board[4] == board[7])
+                || (board[2] == board[5] && board[5] == board[8]));
+    };
+
+    const checkdiags = () => {
+        return ((board[0] == board[4] && board[4] == board[8])
+                || (board[6] == board[4] && board[4] == board[2]));
+    };
+
+    const gameOver = () => {
+        return checkRows() || checkcols() || checkdiags();
+    };
+
+    return { populateBoard, makeMove, gameOver };
 })();
 
-document.querySelector("#pop-test").addEventListener("click", (Event) => {
-    gameBoard.populateBoard();
-});
+gameBoard.populateBoard();
+console.log(gameBoard.gameOver());
